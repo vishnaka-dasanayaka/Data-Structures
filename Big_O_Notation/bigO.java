3a591c4b434bbff8d53d1e5a1ffa5c7ad9fcd175
vishnaka-dasanayaka <vishnakadasanayaka@gmail.com>
2024-08-09 12:54:19 +0530

    O(n^2)

--- Big_O_Notation/bigO.java
+++ Big_O_Notation/bigO.java
@@ -18,13 +18,23 @@ public class bigO {
         for(int number : numbers) System.out.print(number + " ");
     }
 
-
+    // Time complexity is O(n^2) - proportional to the square of number of inputs
+    public static void nestedLoop(int[] numbers){
+        for(int first : numbers){
+            for(int second : numbers){
+                System.out.print(first + " - " + second + "   ");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String [] args){
-        int[] num = {111,2,3,4,6};
+        int[] num = {1,2,3,4,6};
         printArr(num);
         printWithFor(num);
         System.out.println();
         printWithForEach(num);
+        System.out.println();
+        nestedLoop(num);
     }
 }
